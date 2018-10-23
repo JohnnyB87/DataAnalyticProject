65723ee029ea550d7e58481d95ad1fdb71b8f4fe
John Brady R00155390 <john.brady1@mycit.ie>
2018-10-23 19:44:55 +0100

    comments added files added tidied code created a new script that holds the functions associated with the files

--- venv/main.py
+++ venv/main.py
@@ -1,15 +1,28 @@
+# Name: John Brady
+# Student No.: R00155390
+# SDH3-A
+
+# imports
 import numpy as np
 from glob import glob
-import os
+import rainfallFunctions as rf
 
-column = {"Year": 0, "Month": 1, "Total": 2, "Most": 3, "Rain Days": 4}
 
+# main function
 def main():
-    while True:
-        userInput = 0
-        while(userInput < 1 or userInput > 6):
-            printMainMenu()
-            userInput = int(input("Please select one of the above options: "))
+    while True:     # infinite loop
+        userInput = 0   # set initial value for userInput variable
+        while userInput < 1 or userInput > 6:   # loop until a valid number is entered
+            printMainMenu()     # calls function that prints out the main menu
+            try:    # try except to handle wrong input types
+                # gets user input and assigns it to userInput variable
+                # try's to convert input to an integer
+                userInput = int(input("Please select one of the above options: "))
+            except ValueError:  # if conversion fails
+                # assign userInput the value -1 to continue the while loop
+                userInput = -1
+                print("Enter a number.")
+        # if userInput is not 6 call the runOptionMain function
         if userInput != 6:
             runOptionMain(userInput)
         else:
@@ -17,13 +30,7 @@ def main():
             break
 
 
-def testNumpy():
-    test = openFile("Cork")
-    testAll = importAllFiles()
-    print("Total Test: ", np.sum(test[:, column["Most"]], axis=0))
-    print("Total TestAll: ", np.sum(testAll[:, column["Most"]], axis=0))
-
-
+# function to print the main menu
 def printMainMenu():
     print("\n\nMain Menu"
           "\n1. Basic Statistics for Total Rainfall (Millimetres)"
@@ -34,6 +41,7 @@ def printMainMenu():
           "\n6. Exit")
 
 
+# function to print the secondary menu
 def printSecondaryMenu():
     print("\n\n1. Belfast"
           "\n2. Cork"
@@ -42,28 +50,7 @@ def printSecondaryMenu():
           "\n5. Limerick")
 
 
-def runOptionMain(choice):
-    if choice > 0 and choice < 4:
-        userInput = -1
-        while userInput < 0 or userInput > 5:
-            printSecondaryMenu()
-            userInput = int(input("Please select a location: "))
-        county = getCounty(userInput)
-        file = openFile(county)
-        if choice == 1:
-            totalRainfallStats(file)
-        elif choice == 2:
-            mostRainfallStats(file)
-        elif choice == 3:
-            rainDayRainfallStats(file)
-    else:
-        dic = readAllFiles()
-        if choice == 4:
-            totalRainfallStatsAll(dic)
-        else:
-            percentageOfRainfall(dic)
-
-
+# switch statement to get the county associated with the users input
 def getCounty(choice):
     switcher = {
         1: "Belfast",
@@ -75,57 +62,38 @@ def getCounty(choice):
     return switcher.get(choice, "Invalid option")
 
 
-def openFile(county):
-    return np.loadtxt("../rainFallData/" + county + "Rainfall.txt")
-
-def readAllFiles():
-    fnames = glob('../rainFallData/*.txt')
-    fnames.sort()
-
-    dic = {}
-    for f in fnames:
-        sep = ''
-        county = sep.join(f)
-        county = county[16:len(county)-12]
-        dic[county] = np.loadtxt(f)
-
-    return dic
-
-
-def totalRainfallStats(file):
-    print("Most Rainfall: ", np.amax(file[:, column["Total"]], axis=0))
-    print("Average of Most Rainfall: ", np.mean(file[:, column["Total"]], axis=0))
-
-
-def mostRainfallStats(file):
-    print("Most Rainfall: ", np.amax(file[:, column["Most"]], axis=0))
-    print("Average of Most Rainfall: ", np.mean(file[:, column["Most"]], axis=0))
-
-
-def rainDayRainfallStats(file):
-    print("Most Rainfall: ", np.amax(file[:, column["Rain Days"]], axis=0))
-    print("Average of Most Rainfall: ", np.mean(file[:, column["Rain Days"]], axis=0))
-
-
-def totalRainfallStatsAll(dic):
-    max = 0.0
-    county = ""
-    for key, value in dic.items():
-        total = np.sum(dic[key][:, column["Total"]], axis=0)
-        if total > max:
-            max = total
-            county = key
-        print(key, " : ", total)
-    print("Wettest location: ", county, " with ", max, "mm")
+# function that takes the users choice and runs the necessary functions associated with that choice
+def runOptionMain(choice):
+    if 0 < choice < 4:  # if choice is between the values 0 and 4 exclusive
+        userInput = -1  # set initial value for userInput variable
+        while userInput < 0 or userInput > 5:   # loops until a valid choice is entered
+            printSecondaryMenu()    # function to print the secondary menu
+            try:    # try except to handle wrong input types
+                # gets user input and assigns it to userInput variable
+                # try's to convert input to an integer
+                userInput = int(input("Please select a location: "))
+            except ValueError:  # if conversion fails
+                # assign userInput the value -1 to continue the while loop
+                userInput = -1
+                print("Enter a number")
+        county = getCounty(userInput)   # get the county associated with userInput and assign it to a variable
+        file = rf.openFile(county)  # get the text file associated with the county chosen
+        print("\n\n", county)
+        # if else statement to call the function associated with the users input
+        if choice == 1:
+            rf.printColumnData(file, "Total")
+        elif choice == 2:
+            rf.printColumnData(file, "Most")
+        elif choice == 3:
+            rf.printColumnData(file, "Rain Days")
+    else:
+        dic = rf.readAllFiles()
+        if choice == 4:
+            rf.totalRainfallStatsAll(dic)
+        else:
+            rf.percentageOfRainfall(dic)
 
 
-def percentageOfRainfall(dic):
-    threshold = int(input("Please enter maximum threshold value for number of rain days: "))
-    for key, value in dic.items():
-        res = np.count_nonzero(dic[key][:, column["Rain Days"]] <= threshold)
-        tot = np.count_nonzero(dic[key][:, column["Rain Days"]])
-        print('TOT: ' , tot, "RES: ", res )
-        print(key, " : ", res/tot*100,"%")
-
+# call main function
 main()
 
